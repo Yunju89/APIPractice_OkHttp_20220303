34211e1e575258d539e0c96e0739d239930bc7bf
Yunju89 <toyaann@naver.com>
2022-03-03 17:00:35 +0900

    xml / kt 연결 변수 - binding 변수 생성

--- app/src/main/java/org/techtown/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/org/techtown/apipractice_okhttp_20220303/MainActivity.kt
@@ -2,10 +2,15 @@ package org.techtown.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.techtown.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding              // xml 에 <layout> 그리고 자동완성
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
