fa8946cf680683b5bb76630608d8d3687214f870
Yunju89 <toyaann@naver.com>
2022-03-04 15:59:03 +0900

    추상메쏘드 2개를 물려주는 클래스 생성.  - 추상 클래스로 생성.(xml 만들 필요x)

--- app/src/main/java/org/techtown/apipractice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/org/techtown/apipractice_okhttp_20220303/SignUpActivity.kt
@@ -12,5 +12,11 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+
+
     }
+
+
+
+
 }
