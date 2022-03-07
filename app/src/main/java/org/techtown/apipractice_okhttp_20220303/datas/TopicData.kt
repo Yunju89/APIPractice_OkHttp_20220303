4c63e6ee09f7298f6fe1b093f8651c548c94c0ba
Yunju89 <toyaann@naver.com>
2022-03-07 17:09:48 +0900

    토론 주제 표현용 데이터 클래스 추가  - 생성자 X, 멤버변수만 추가  - 서버의 데이터를 참고해서 추가

---
+++ app/src/main/java/org/techtown/apipractice_okhttp_20220303/datas/TopicData.kt
@@ -0,0 +1,12 @@
+package org.techtown.apipractice_okhttp_20220303.datas
+
+class TopicData{
+
+    var id = 0  // id는 Int 라고 명시
+    var title = ""  // title String
+    var imageUrl = ""  // 서버 : img_url, 앱 imageUrl 변수명 다른 경우
+    var replyCount = 0
+
+
+
+}
