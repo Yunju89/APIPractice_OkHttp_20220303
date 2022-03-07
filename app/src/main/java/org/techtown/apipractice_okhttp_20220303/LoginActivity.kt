f34d1d2619da5f62ad9d1a9b33a3d4c9cb03be54
Yunju89 <toyaann@naver.com>
2022-03-07 15:33:48 +0900

    체크박스의 이벤트 처리 체험

--- app/src/main/java/org/techtown/apipractice_okhttp_20220303/LoginActivity.kt
+++ app/src/main/java/org/techtown/apipractice_okhttp_20220303/LoginActivity.kt
@@ -2,6 +2,7 @@ package org.techtown.apipractice_okhttp_20220303
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import org.json.JSONObject
@@ -23,6 +24,13 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        
+//        체크박스의 체크 여부가 변경되면 > ContextUtil 이용, 체크 값 저장
+        binding.autoLoginChekBox.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            Log.d("체크값변경", "${isChecked}로 변경됨됨")
+
+       }
 
         binding.btnSignUp.setOnClickListener {
 
