10df7efacde4c9d4ceb1a3a002c849b8ad810874
Yunju89 <toyaann@naver.com>
2022-03-08 17:29:45 +0900

    UserData 파싱 클래스 추가 + ReplyData 파싱에 활용  - 리스트뷰에 사용할 데이터 클래스 준비작업.

--- app/src/main/java/org/techtown/apipractice_okhttp_20220303/datas/ReplyData.kt
+++ app/src/main/java/org/techtown/apipractice_okhttp_20220303/datas/ReplyData.kt
@@ -6,6 +6,13 @@ class ReplyData(
     var id : Int,
     var content: String,
 ) {
+
+    var writer = UserData() // 모든 댓글에는 작성자가 있다. null 가능성이 없다.
+
+
+
+
+
     
 //    보조 생성자 추가 연습 : 파라미터 X
     constructor() : this( 0, "내용없음")
@@ -16,6 +23,14 @@ class ReplyData(
         fun getReplyDataFromJson(jsonObj : JSONObject) : ReplyData {
 
             val replyData = ReplyData()
+
+//            JSON 정보 > 멤버변수 채우기
+
+            replyData.id = jsonObj.getInt("id")
+            replyData.content = jsonObj.getString("content")
+            replyData.writer = UserData.getUserDataFromJson( jsonObj.getJSONObject("user") )
+
+
 
                 return replyData
 
