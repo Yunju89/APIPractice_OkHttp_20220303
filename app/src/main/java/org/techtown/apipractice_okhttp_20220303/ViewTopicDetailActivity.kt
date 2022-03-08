2c2b4edec3a801da3a3ba87ea8ef2702382084c1
Yunju89 <toyaann@naver.com>
2022-03-08 17:51:34 +0900

    토론 상세 정보 조회시, 댓글 목록도 같이 파싱 > 별도 ArrayList에 담아주기

--- app/src/main/java/org/techtown/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
+++ app/src/main/java/org/techtown/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
@@ -7,6 +7,7 @@ import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import org.json.JSONObject
 import org.techtown.apipractice_okhttp_20220303.databinding.ActivityViewTopicDetailBinding
+import org.techtown.apipractice_okhttp_20220303.datas.ReplyData
 import org.techtown.apipractice_okhttp_20220303.datas.TopicData
 import org.techtown.apipractice_okhttp_20220303.utils.ServerUtil
 
@@ -16,6 +17,8 @@ class ViewTopicDetailActivity : BaseActivity() {
     //    보여주게 될 토론 주제 데이터 > 이벤트처리, 데이터 표현 등 여러 함수에서 사용
 //    화면에 들어와서 > Intent 통해 대입 (멤버변수에 대입을 늦게하고 싶다> 일반 멤버변수는 초기값 세팅을 꼭 해야하기때문에)
     lateinit var mTopicData: TopicData
+
+    var mReplyList = ArrayList<ReplyData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,6 +84,7 @@ class ViewTopicDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
         getTopicDetailFromServer()
         setTopicDataToUi()
 
@@ -142,16 +146,26 @@ class ViewTopicDetailActivity : BaseActivity() {
 //                    토론 정보 JSONObject (topicObj) => TopicData() 형태로 변환 (여러 화면에서 진행. 함수로 만들어두자)
                     val topicData = TopicData.getTopicDataFromJson(topicObj)
 
+//                    변환된 객체를 mTopicData 로 다시 대입 => UI 반영도 다시 실행
                     mTopicData = topicData
 
                     runOnUiThread {
                         setTopicDataToUi()
                     }
 
+//                    topicObj 내부에는 replies 라는 댓글 목록 JSONArray 도 들어있다.
+//                    mReplyList 에 넣어주자.
 
-//                    변환된 객체를 mTopicData 로 다시 대입 => UI 반영도 다시 실행
+                    val repliesArr = topicObj.getJSONArray("replies")
 
+                    for (i in 0 until repliesArr.length()){
+                        val replyObj = repliesArr.getJSONObject(i)
 
+                        mReplyList.add( ReplyData.getReplyDataFromJson(replyObj) )
+
+                    }
+
+//                    서버의 동작이므로, 어댑터 세팅보다 늦게 끝날 수 있다. (notifyDataSetChanged)
                 }
 
             })
