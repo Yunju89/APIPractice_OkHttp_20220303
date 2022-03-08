1a5af0010e4c76f9d30bfc275496dcee2c81afcf
Yunju89 <toyaann@naver.com>
2022-03-08 11:38:28 +0900

    진입한 화면의 기본 UI/데이터 세팅

--- app/src/main/java/org/techtown/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
+++ app/src/main/java/org/techtown/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
@@ -3,6 +3,7 @@ package org.techtown.apipractice_okhttp_20220303
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import org.techtown.apipractice_okhttp_20220303.databinding.ActivityViewTopicDetailBinding
 import org.techtown.apipractice_okhttp_20220303.datas.TopicData
 
@@ -10,7 +11,7 @@ class ViewTopicDetailActivity : BaseActivity() {
     lateinit var binding : ActivityViewTopicDetailBinding
 
 //    보여주게 될 토론 주제 데이터 > 이벤트처리, 데이터 표현 등 여러 함수에서 사용
-//    화면에 들어와서 > Intent 통해 대입
+//    화면에 들어와서 > Intent 통해 대입 (멤버변수에 대입을 늦게하고 싶다> 일반 멤버변수는 초기값 세팅을 꼭 해야하기때문에)
     lateinit var mTopicData : TopicData
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +30,8 @@ class ViewTopicDetailActivity : BaseActivity() {
 
     override fun setValues() {
 
+        binding.txtTitle.text = mTopicData.title
+        Glide.with(mContext).load(mTopicData.imageUrl).into(binding.imgTopicBackground)
     }
 
 
