07f6d4b0421b5d5cd5971d67c03c225a99a639a3
Yunju89 <toyaann@naver.com>
2022-03-08 17:21:09 +0900

    댓글 데이터 제작 중 - 보조 생성자 개념 체험

---
+++ app/src/main/java/org/techtown/apipractice_okhttp_20220303/datas/ReplyData.kt
@@ -0,0 +1,25 @@
+package org.techtown.apipractice_okhttp_20220303.datas
+
+import org.json.JSONObject
+
+class ReplyData(
+    var id : Int,
+    var content: String,
+) {
+    
+//    보조 생성자 추가 연습 : 파라미터 X
+    constructor() : this( 0, "내용없음")
+
+
+    companion object{
+
+        fun getReplyDataFromJson(jsonObj : JSONObject) : ReplyData {
+
+            val replyData = ReplyData()
+
+                return replyData
+
+        }
+    }
+
+}
